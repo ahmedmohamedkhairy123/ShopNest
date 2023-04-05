10282f4423980612f87c0ea11c3e7995ffdec14d
ahmedmohamedkhairy123 <ahmedmohamedkhairy123@gmail.com>
2023-04-05 10:12:00 +0200

    Phase 4: Added LocalStorage persistence

--- script.js
+++ script.js
@@ -1,33 +1,13 @@
 // 1. Product Data
 const products = [
-    {
-        id: 1,
-        name: "Minimalist Watch",
-        price: 120.00,
-        image: "https://images.unsplash.com/photo-1523275335684-37898b6baf30?auto=format&fit=crop&w=500&q=60"
-    },
-    {
-        id: 2,
-        name: "Wireless Headphones",
-        price: 85.50,
-        image: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?auto=format&fit=crop&w=500&q=60"
-    },
-    {
-        id: 3,
-        name: "Polaroid Camera",
-        price: 250.00,
-        image: "https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?auto=format&fit=crop&w=500&q=60"
-    },
-    {
-        id: 4,
-        name: "Classic Sunglasses",
-        price: 45.00,
-        image: "https://images.unsplash.com/photo-1572635196237-14b3f281503f?auto=format&fit=crop&w=500&q=60"
-    }
+    { id: 1, name: "Minimalist Watch", price: 120.00, image: "https://images.unsplash.com/photo-1523275335684-37898b6baf30?auto=format&fit=crop&w=500&q=60" },
+    { id: 2, name: "Wireless Headphones", price: 85.50, image: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?auto=format&fit=crop&w=500&q=60" },
+    { id: 3, name: "Polaroid Camera", price: 250.00, image: "https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?auto=format&fit=crop&w=500&q=60" },
+    { id: 4, name: "Classic Sunglasses", price: 45.00, image: "https://images.unsplash.com/photo-1572635196237-14b3f281503f?auto=format&fit=crop&w=500&q=60" }
 ];
 
-// 2. The Cart Array (State)
-let cart = [];
+// 2. Load Cart from Local Storage (or empty array)
+let cart = JSON.parse(localStorage.getItem('shopnest_cart')) || [];
 
 // 3. Select DOM Elements
 const productList = document.getElementById('product-list');
@@ -53,22 +33,23 @@ function displayProducts() {
 
 // 5. Add to Cart Logic
 function addToCart(productId) {
-    const productToAdd = products.find(p => p.id === productId);
-    cart.push(productToAdd);
+    const product = products.find(p => p.id === productId);
+    cart.push(product);
 
-    console.log("Cart is now:", cart);
-    updateCartCount();
+    saveCart();       // Save to storage
+    updateCartCount(); // Update UI
 }
 
-// 6. Update Cart Count UI
+// 6. Save Logic
+function saveCart() {
+    localStorage.setItem('shopnest_cart', JSON.stringify(cart));
+}
+
+// 7. Update Cart Count UI
 function updateCartCount() {
     cartCountElement.innerText = cart.length;
-
-    cartCountElement.classList.add('bump');
-    setTimeout(() => {
-        cartCountElement.classList.remove('bump');
-    }, 300);
 }
 
-// Initialize the app
+// Initialize
 displayProducts();
+updateCartCount();
